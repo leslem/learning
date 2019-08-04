aba69c94c381d8796a75b486387bc21bc38bdf1a
Leslie Emery <leslie.s.emery@gmail.com>
2019-08-04 15:16:12 -0700

    Add pip sync command to DSFS mkenv script

--- books/data-science-from-scratch/mkenv.sh
+++ books/data-science-from-scratch/mkenv.sh
@@ -6,3 +6,6 @@ mkvirtualenv -r requirements.txt -a . --python=python3.7 ds-from-scratch
 
 # Compile a pinned requirements file.
 pip-compile requirements.in
+
+# Install packages.
+pip-sync requirements.txt
