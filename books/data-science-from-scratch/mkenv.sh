7e61f09d34f17c334bca10f9eda0c537db731728
Leslie Emery <leslie.s.emery@gmail.com>
2019-08-03 21:53:38 -0700

    Set up virtualenv for Data Science From Scratch

---
+++ books/data-science-from-scratch/mkenv.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Create a virtualenv with the desired packages
+# and set the project directory for `workon`.
+mkvirtualenv -r requirements.txt -a . --python=python3.7 ds-from-scratch
+
+# Compile a pinned requirements file.
+pip-compile requirements.in
