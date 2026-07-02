f0f95e099c8beecc7bc12836cbcb19bc8bf6cc56
Leslie Emery <leslie.s.emery@gmail.com>
2026-07-02 10:40:06 -0700

    Use native R pipe and Air formatter in Tidy Modeling with R project

--- R/books/tidy-modeling-with-r/ch04.R
+++ R/books/tidy-modeling-with-r/ch04.R
@@ -5,8 +5,8 @@ data(ames, package = "modeldata")
 
 glimpse(ames)
 ggplot(ames, aes(x = Sale_Price)) +
-    geom_histogram(bins = 50, col = "white") +
-    scale_x_log10()
+  geom_histogram(bins = 50, col = "white") +
+  scale_x_log10()
 
 ames <- ames |>
-    mutate(Sale_Price = log10(Sale_Price))
+  mutate(Sale_Price = log10(Sale_Price))
